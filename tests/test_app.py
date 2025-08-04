0b0bc1444c2ca2b40025e99dbb253b34a79028ac
Magdalena Getka <getka.magdalena@gmail.com>
2025-08-04 11:00:54 +0200

    tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,24 +7,24 @@ def client():
     app.config["TESTING"] = True
     test_client = app.test_client()
 
-    # Wyczyść bazę danych przed każdym testem
+    # Deleteing database before testing
     todos.delete_many({})
     yield test_client
-    todos.delete_many({})  # również po
+    todos.delete_many({})  # deleteing database after testing
 
 def test_add_task(client):
-    response = client.post("/add", data={"task": "Zadanie testowe"})
+    response = client.post("/add", data={"task": "New task"})
     assert response.status_code == 302  # redirect
     assert todos.count_documents({}) == 1
 
 def test_index_page(client):
-    todos.insert_one({"task": "Zadanie A", "done": False})
+    todos.insert_one({"task": "Task A", "done": False})
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Zadanie A" in response.data
+    assert b"Task A" in response.data
 
 def test_complete_task(client):
-    task = {"task": "Do zrobienia", "done": False}
+    task = {"task": "Task B", "done": False}
     inserted = todos.insert_one(task)
     task_id = str(inserted.inserted_id)
 
